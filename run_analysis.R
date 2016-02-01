7ad2568f81b339fc87ef3d6d34f83aae94870cc2
Ian Alexander <ian.m.alexander@gmail.com>
2016-02-01 00:52:50 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,17 @@
 ## import test data files, name the coloumns using the features list, then merge the files.
-X_test <- read.table("~/R Testing/Smarthphones/test/X_test.txt", quote="\"", comment.char="")
-y_test <- read.table("~/R Testing/Smarthphones/test/y_test.txt", quote="\"", comment.char="")
-sub_test <- read.table("~/R Testing/Smarthphones/test/subject_test.txt", quote="\"", comment.char="")
-varnames <- read.table("~/R Testing/Smarthphones/features.txt", quote="\"", comment.char="")
+X_test <- read.table("~/test/X_test.txt", quote="\"", comment.char="")
+y_test <- read.table("~/test/y_test.txt", quote="\"", comment.char="")
+sub_test <- read.table("~/test/subject_test.txt", quote="\"", comment.char="")
+varnames <- read.table("~/features.txt", quote="\"", comment.char="")
 colnames(X_test) <- varnames[,2]
 colnames(y_test) <- "Activity"
 colnames(sub_test) <- "Subject"
 test <- cbind(X_test, sub_test, y_test)
 
 ## import train data files, name the coloumns using the features list, then merge the files.
-X_train <- read.table("~/R Testing/Smarthphones/train/X_train.txt", quote="\"", comment.char="")
-y_train <- read.table("~/R Testing/Smarthphones/train/y_train.txt", quote="\"", comment.char="")
-sub_train <- read.table("~/R Testing/Smarthphones/train/subject_train.txt", quote="\"", comment.char="")
+X_train <- read.table("~/train/X_train.txt", quote="\"", comment.char="")
+y_train <- read.table("~/train/y_train.txt", quote="\"", comment.char="")
+sub_train <- read.table("~/train/subject_train.txt", quote="\"", comment.char="")
 colnames(X_train) <- varnames[,2]
 colnames(y_train) <- "Activity"
 colnames(sub_train) <- "Subject"
@@ -43,5 +43,6 @@ names(tidy) <- gsub("jerk", " jerk", names(tidy))
 names(tidy) <- gsub("\\()", "", names(tidy))
 
 ## Save a version with the mean for each subject and activity
+temp <- 
 tidymean <- aggregate(. ~ subject + activity, mean, data = tidy)
-write.table(tidymean, "~/R Testing/Smarthphones/tidymeans.csv", row.name=FALSE)
+write.table(tidymean, "~/tidymeans.txt", row.name=FALSE)
